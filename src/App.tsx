1f8086715e2d25f0b0e62f7290d142f4bb91786d
Selena Siri <selenasiri@gmail.com>
2021-02-06 20:37:28 -0500

    piano sounds from mp3

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,12 @@ import Octave from './Octave'
 function App() {
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     console.log(e.currentTarget.value);
-  }
+    // Create audio here
+    const audio = new Audio(`sounds/piano_${e.currentTarget.value}.mp3`);
+
+    // Then we want to play it
+    audio.play();
+  };
 
   return (
     <Wrapper>
